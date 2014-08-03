8b5869d91ad6e8128a51837735702fbb64a87df5
dguan <gdyxgzy@hotmail.com>
2014-08-03 13:51:25 +1000

    extended missionary-cannible problem, find optimal solution from all possible solutions. Returning all solutions trying to find out how big the solution space is.

---
+++ misc-prjs/river-crossing/extended-missionary/missionary2.cpp
@@ -0,0 +1,495 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <numeric>
+#include <algorithm>
+#include <cassert>
+
+template<class BiItor> bool next_combination_idx(int n, BiItor comb_first, BiItor comb_last)
+{
+	int k = std::distance(comb_first, comb_last);
+	assert(n >= k && k > 0);	// Obviously if k==0 there is no next combination
+
+	BiItor cur_pos = comb_last;
+	--cur_pos;
+	int chg_max = n - 1;
+	while (true)
+	{
+		auto cur_idx = *cur_pos + 1;
+		if (chg_max >= cur_idx)
+		{
+			while (cur_pos != comb_last)
+				*cur_pos++ = cur_idx++;
+			return true;
+		}
+		else
+		{
+			if (cur_pos != comb_first)
+			{
+				--cur_pos;
+				--chg_max;
+			}
+			else
+				return false;
+		}
+	}
+}
+
+std::vector<std::vector<int>> get_all_combs_idx(int n, int k)
+{
+	std::vector<std::vector<int>> all_combs;
+	std::vector<int> cur_comb(k);
+	std::iota(cur_comb.begin(), cur_comb.end(), 0);
+	do {
+		all_combs.push_back(cur_comb);
+	} while (next_combination_idx(n, cur_comb.begin(), cur_comb.end()));
+
+	return all_combs;
+}
+
+std::vector<std::vector<int>> get_multi_comb_idx_bits(int *grp_cnts, int num_grps, int combo_cnt)
+{
+	std::vector<std::vector<int>> results;
+
+	if (num_grps == 1)
+	{
+		if (combo_cnt == 0)
+			results.push_back(std::vector<int>(1, 0));
+		else if (grp_cnts[0] >= combo_cnt)
+		{
+			for (auto vi : get_all_combs_idx(grp_cnts[0], combo_cnt))
+			{
+				int this_grp_comb = 0;
+				for (int i : vi)
+					this_grp_comb |= (1 << i);
+				results.push_back(std::vector<int>(1, this_grp_comb));
+			}
+		}
+	}
+	else if (combo_cnt == 0)
+	{
+		std::vector<int> temp;
+		for (int i = 0; i < num_grps; ++i)
+			temp.push_back(0);
+		results.push_back(std::move(temp));
+		return results;
+	}
+	else
+	{
+		for (int i = std::min(combo_cnt, grp_cnts[0]); i >= 0; --i)
+		{
+			std::vector<std::vector<int>> comb_0;
+			if (i == 0)
+				comb_0.push_back(std::vector<int>());
+			else
+				comb_0 = get_all_combs_idx(grp_cnts[0], i);
+
+			for (auto c : comb_0)
+			{
+				int grp_cnt_0 = 0;
+				for (int i : c)
+					grp_cnt_0 |= (1 << i);
+				for (auto vi : get_multi_comb_idx_bits(grp_cnts + 1, num_grps - 1, combo_cnt - i))
+				{
+					vi.insert(vi.begin(), grp_cnt_0);
+					results.push_back(std::move(vi));
+				}
+			}
+		}
+	}
+	return results;
+}
+
+
+// Both DFS and BFS give out correct answer 20, but,
+// DFS has only 30 solutions, while BFS has 8100!
+// Make sure to have the 2 tables sorted first!
+//const std::vector<int> missionaries{ 4, 5, 6 };
+//const std::vector<int> cannibles{ 1, 2, 3 };
+//const int boat_volume = 2;
+
+// Both DFS and BFS give out correct answer 28, but, DFS solutions
+// increased to 264, while BFS solutions jumped to 944784! So it will
+// take tons of time even to print all these solutions.
+//const std::vector<int> missionaries{ 4, 5, 6, 7 };
+//const std::vector<int> cannibles{ 2, 3, 5 };
+//const int boat_volume = 2;
+
+// There is no solution when M=C=4 and boat_volume=2
+//const std::vector<int> missionaries{ 6, 8, 10, 12 };
+//const std::vector<int> cannibles{ 3, 5, 7, 9 };
+//const int boat_volume = 2;
+
+// Never run BFS algorithm on this!
+// Best time 11, 368 DFS solutions.
+//const std::vector<int> missionaries{ 2, 4, 5, 6 };
+//const std::vector<int> cannibles{ 1, 2, 3, 4 };
+//const int boat_volume = 3;
+
+// Total 1584 DFS solutions, best time 15
+//const std::vector<int> missionaries{ 2, 4, 5, 6, 8 };
+//const std::vector<int> cannibles{ 1, 2, 3, 4, 5 };
+//const int boat_volume = 3;
+
+// Total 73086 DFS solutions, best time 9, one of the best solution is:
+// m2m4c1c2-> m2c1<- m2m5c1c3-> m2c1<- m2m5c1c4-> m5c1<- m6m8c1c5-> m6c1<- m5m6c1c6->
+const std::vector<int> missionaries{ 2, 4, 5, 5, 6, 8 };
+const std::vector<int> cannibles{ 1, 2, 3, 4, 5, 6 };
+const int boat_volume = 4;
+
+struct state
+{
+	unsigned int mLeft;
+	unsigned int cLeft;
+	unsigned int mRight;
+	unsigned int cRight;
+	bool boat_side;			// boat's side, false for left side, true for right side
+	std::string movs;		// the moves till this state
+	int time;				// the time till this state
+	state(unsigned int mL, unsigned int cL, unsigned int mR, unsigned int cR, bool side, std::string mv, int tm)
+		: mLeft(mL), cLeft(cL), mRight(mR), cRight(cR), boat_side(side), movs(mv), time(tm) {};
+};
+
+typedef std::unordered_map<unsigned long long, int> UsedStateTable;
+
+const int INTEGER_BITS = sizeof(int)* 8;// Total bits in an integer, maximum elements in an integer should be INTEGER_BITS-1
+static int g_Missionary_N;				// number of missionaries
+static int g_Cannible_N;				// number of cannibles, this may be less than the missionaries
+
+unsigned char BIT_CNT[256];		// number of ones in a char, used to check ones in integers quickly
+inline void init_bit_cnt_table(unsigned char table[])	// initialize bit-count table
+{
+	unsigned char bit_cnt_4bits[] = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 };
+	for (int i = 0; i < 16; ++i)
+	for (int j = 0; j < 16; ++j)
+		table[i * 16 + j] = bit_cnt_4bits[i] + bit_cnt_4bits[j];
+}
+inline int count_ones(unsigned int x)	// count the ones in an unsigned integer
+{
+	int total_bits = 0;
+	for (unsigned int i = 0; i < sizeof(int); ++i)
+	{
+		total_bits += BIT_CNT[x & 0xff];
+		x >>= 8;
+	}
+	return total_bits;
+}
+
+inline bool	is_safe_mov(int nml, int ncl, int nmr, int ncr)	// check if a move safe or not
+{
+	return (!nml || nml >= ncl) && (!nmr || nmr >= ncr);
+}
+
+inline bool solution_found(const state& st, int boat_volume)	// check if we have found a solution
+{
+	return (count_ones(st.mLeft)+count_ones(st.cLeft) <= boat_volume) && (st.boat_side == false);
+}
+
+// By checking bit_order, convert the bit index in real order in missionary or cannible table index
+std::vector<int> bit_index_to_index(unsigned int bit_order, int max_bits, int bit_index)
+{
+	std::vector<int> result;
+
+	std::vector<int> int_index;	// change bit_index's bit index into integer index to lookup the bit_order
+
+	unsigned int bit_mask = 1;
+	unsigned int max_bit_mask = (1 << max_bits);
+	int bit_pos = 0;
+	while (bit_mask < max_bit_mask)
+	{
+		if (bit_mask & bit_index)
+			int_index.push_back(bit_pos);
+		bit_mask <<= 1;
+		++bit_pos;
+	}
+
+	bit_mask = 1;
+	bit_pos = 0;
+	unsigned int cnt = 0;
+	int bit_idx_cnt = -1;
+	while (cnt < int_index.size())
+	{
+		int idx = int_index[cnt];
+		while (bit_mask < max_bit_mask)
+		{
+			if (bit_mask & bit_order)
+			{
+				++bit_idx_cnt;
+				if (bit_idx_cnt == idx)
+				{
+					result.push_back(bit_pos);
+					bit_mask <<= 1;
+					++bit_pos;
+					break;
+				}
+			}
+			bit_mask <<= 1;
+			++bit_pos;
+		}
+		++cnt;
+	}
+	return result;
+}
+
+// Filter unsafe next states and get all safe next states
+// If a state is previously visited and has shorter time, it will also be filtered out 
+std::vector<state> get_safe_movs(const state& st, int boat_volume, const UsedStateTable& used_states)
+{
+	std::vector<state> results;
+	std::vector<std::vector<int>> possible_combos;
+	int grps[2];
+
+	grps[0] = st.boat_side ? count_ones(st.mRight) : count_ones(st.mLeft);
+	grps[1] = st.boat_side ? count_ones(st.cRight) : count_ones(st.cLeft);
+
+	if (st.boat_side)	// Search strategy, if the boat is at the other side, choose fewer people first
+	{
+		for (int i = 1; i <= boat_volume; ++i)
+		for (auto gc : get_multi_comb_idx_bits(grps, 2, i))
+			possible_combos.push_back(std::move(gc));
+	}
+	else // Search strategy, try to take as many as possible people to the other side first
+	{
+		for (int i = boat_volume; i >= 1; --i)
+		for (auto gc : get_multi_comb_idx_bits(grps, 2, i))
+			possible_combos.push_back(std::move(gc));
+	}
+
+	for (auto pc : possible_combos)
+	{
+		unsigned int ml = st.mLeft;
+		unsigned int cl = st.cLeft;
+		unsigned int mr = st.mRight;
+		unsigned int cr = st.cRight;
+
+		unsigned int & m_from = st.boat_side ? mr : ml;
+		unsigned int & m_to = st.boat_side ? ml : mr;
+		unsigned int & c_from = st.boat_side ? cr : cl;
+		unsigned int & c_to = st.boat_side ? cl : cr;
+
+		int min_m_time = std::numeric_limits<int>::max();
+		int min_c_time = std::numeric_limits<int>::max();
+
+		std::vector<int> m_bit_index = std::move(bit_index_to_index(m_from, g_Missionary_N, pc[0]));
+		std::vector<int> c_bit_index = std::move(bit_index_to_index(c_from, g_Cannible_N, pc[1]));
+
+		if (!m_bit_index.empty())
+			min_m_time = missionaries[m_bit_index[0]];	//because missionaries and m_bit_index are all sorted, so the first is the shortest in time
+		if (!c_bit_index.empty())
+			min_c_time = cannibles[c_bit_index[0]];
+
+		for (int i : m_bit_index)	// move the missionaries from this state to next state
+		{
+			m_from &= (~(1<<i));
+			m_to |= (1 << i);
+		}
+		for (int i : c_bit_index)	// move the cannibles from this state to next state
+		{
+			c_from &= (~(1 << i));
+			c_to |= (1 << i);
+		}
+		// check if this next state is safe or not
+		if (!is_safe_mov(count_ones(ml), count_ones(cl), count_ones(mr), count_ones(cr)))
+			continue;
+
+		bool found = false;
+		unsigned long long key = ml;	// construct HashKey
+		key <<= INTEGER_BITS;
+		key |= ((cl << 1) | (st.boat_side ? 0 : 0x1));
+		auto iter = used_states.find(key);
+		if (iter != used_states.end())
+		{
+			if (st.time + std::min(min_m_time, min_c_time) >= iter->second)
+				found = true;
+		}
+		if (!found)
+		{
+			std::string mov(" ");
+			for (int i : m_bit_index)
+				mov = mov + 'm' + std::to_string(missionaries[i]);
+			for (int i : c_bit_index)
+				mov = mov + 'c' + std::to_string(cannibles[i]);
+
+			results.emplace_back(ml, cl, mr, cr, !st.boat_side, st.movs + mov + (st.boat_side ? "<- " : "-> "), st.time + std::min(min_m_time, min_c_time));
+		}
+	}
+	return results;
+}
+
+std::vector<std::tuple<int, std::string>> find_solution_dfs(const state& cur_st, int boat_volume, UsedStateTable& used_states)
+{
+	std::vector<std::tuple<int, std::string>> solutions;
+	if (solution_found(cur_st, boat_volume))
+	{
+		int min_m_time = std::numeric_limits<int>::max();
+		int min_c_time = std::numeric_limits<int>::max();
+		std::string s = cur_st.movs + " ";
+		for (int i = 0; i < g_Missionary_N; ++i)
+		{
+			if (cur_st.mLeft & (1 << i))
+			{
+				int time = missionaries[i];
+				s = s + 'm' + std::to_string(time);
+				if (time < min_m_time)
+					min_m_time = time;
+			}
+		}
+		for (int i = 0; i < g_Cannible_N; ++i)
+		{
+			if (cur_st.cLeft & (1 << i))
+			{
+				int time = cannibles[i];
+				s = s + 'c' + std::to_string(time);
+				if (time < min_c_time)
+					min_c_time = time;
+			}
+		}
+		solutions.emplace_back(cur_st.time + std::min(min_m_time, min_c_time),  s + "-> ");
+		return solutions;
+	}
+	else
+	{
+		bool found = false;
+		unsigned long long key = cur_st.mLeft;
+		key <<= INTEGER_BITS;
+		key |= ((cur_st.cLeft << 1) | (cur_st.boat_side ? 0x1 : 0));
+		auto iter = used_states.find(key);
+		if(iter != used_states.end())
+		{
+			if (cur_st.time < iter->second)
+				iter->second = cur_st.time;
+			found = true;
+		}
+		if (!found)
+			used_states.emplace(key, cur_st.time);
+		for (auto gsm : get_safe_movs(cur_st, boat_volume, used_states))
+		{
+			for(auto tpl : find_solution_dfs(gsm, boat_volume, used_states))
+				solutions.push_back(std::move(tpl));
+		}
+	}
+	return solutions;
+}
+
+std::vector<std::tuple<int, std::string>> find_solution_bfs(std::queue<state>& cur_states, int boat_volume, UsedStateTable& used_states)
+{
+	std::vector<std::tuple<int, std::string>> solutions;
+	while (!cur_states.empty())
+	{
+		state st = cur_states.front();
+		cur_states.pop();
+		bool found = false;
+		unsigned long long key = st.mLeft;
+		key <<= INTEGER_BITS;
+		key |= ((st.cLeft << 1) | (st.boat_side ? 0x1 : 0));
+		auto iter = used_states.find(key);
+		if (iter != used_states.end())
+		{
+			if (st.time < iter->second)
+				iter->second = st.time;
+			found = true;
+		}
+		if (!found)
+			used_states.emplace(key, st.time);
+
+		for (auto gc : get_safe_movs(st, boat_volume, used_states))
+		{
+			if (solution_found(gc, boat_volume))
+			{
+				int min_m_time = std::numeric_limits<int>::max();
+				int min_c_time = std::numeric_limits<int>::max();
+				std::string s = gc.movs + " ";
+				for (int i = 0; i < g_Missionary_N; ++i)
+				{
+					if (gc.mLeft & (1 << i))
+					{
+						int time = missionaries[i];
+						s = s + 'm' + std::to_string(time);
+						if (time < min_m_time)
+							min_m_time = time;
+					}
+				}
+				for (int i = 0; i < g_Cannible_N; ++i)
+				{
+					if (gc.cLeft & (1 << i))
+					{
+						int time = cannibles[i];
+						s = s + 'c' + std::to_string(time);
+						if (time < min_c_time)
+							min_c_time = time;
+					}
+				}
+				solutions.push_back(std::tuple<int, std::string>(gc.time + std::min(min_m_time, min_c_time), s + "-> "));
+			}
+			else
+				cur_states.push(std::move(gc));
+		}
+	}
+	return solutions;
+}
+
+
+int main(void)
+{
+	for (auto vi : get_all_combs_idx(5, 3))
+	{
+		for (auto i : vi)
+			std::cout << i << ",  ";
+		std::cout << std::endl;
+	}
+	int grp_cnts[] = { 3, 2, 2 };
+	std::vector<std::vector<int>> multi_idxs_bits = get_multi_comb_idx_bits(grp_cnts, 3, 3);
+	for (auto vi : multi_idxs_bits)
+	{
+		for (auto i : vi)
+		{
+			std::cout << "[ ";
+			for (unsigned int idx = 0; idx<8 * sizeof(int); ++idx)
+			if (i & (1 << idx))
+				std::cout << idx << "  ";
+			std::cout << "];  ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
+	// Some initialization
+	init_bit_cnt_table(BIT_CNT);
+	g_Missionary_N = missionaries.size();
+	g_Cannible_N = cannibles.size();
+
+	std::cout << std::endl << "******************** DFS algorithm *********************" << std::endl << std::endl;
+
+	UsedStateTable used_states;
+	auto dfs_solutions = find_solution_dfs(state((1<<missionaries.size())-1, (1<<cannibles.size())-1, 0, 0, false, "", 0), boat_volume, used_states);
+
+	std::sort(dfs_solutions.begin(), dfs_solutions.end(), [&](const std::tuple<int, std::string>& a, const std::tuple<int, std::string>& b) { return std::get<0>(a) < std::get<0>(b); });
+	for (auto x : dfs_solutions)
+		std::cout << std::get<0>(x) << " :: " << std::get<1>(x) << std::endl << std::endl;
+/*
+	std::cout << std::endl << "******************** BFS algorithm *********************" << std::endl << std::endl;
+
+	std::queue<state> q_states;
+	used_states.clear();
+	q_states.push(state((1<<missionaries.size())-1, (1<<cannibles.size())-1, 0, 0, false, "", 0));
+	auto bfs_solutions = find_solution_bfs(q_states, boat_volume, used_states);
+
+	std::sort(bfs_solutions.begin(), bfs_solutions.end(), [&](const std::tuple<int, std::string>& a, const std::tuple<int, std::string>& b) { return std::get<0>(a) < std::get<0>(b); });
+	for (auto x : bfs_solutions)
+		std::cout << std::get<0>(x) << " :: " << std::get<1>(x) << std::endl << std::endl;
+*/
+	if (!dfs_solutions.empty())
+	{
+		std::cout << "***** Total " << dfs_solutions.size() << " DFS solutions *****" << std::endl << std::endl;
+		//std::cout << "***** Total " << bfs_solutions.size() << " BFS solutions *****" << std::endl << std::endl;
+		std::cout << "The best DFS solution is: " << std::get<0>(dfs_solutions[0]) << " :: " << std::get<1>(dfs_solutions[0]) << std::endl << std::endl;
+		//std::cout << "The best BFS solution is: " << std::get<0>(bfs_solutions[0]) << " :: " << std::get<1>(bfs_solutions[0]) << std::endl << std::endl;
+	}
+	else
+		std::cout << "No Solution." << std::endl;
+
+	return 0;
+}
---
+++ misc-prjs/river-crossing/extended-missionary/readme.txt
@@ -0,0 +1,19 @@
+This is an extended classic missionary-cannible problem, not only find out the way to go across the river, but, because each missionary and cannible takes different time to row the boat across the river, we would like to find out the optimal way that takes the least time to across the river.
+
+There is a very resonable presumption that, if multiple people in the boat, the time to get to the other side is equal to the minimum time of these people.
+
+This time is much complex than the classic missionary-cannible problem, because that, there are more combinations than the classic problem.
+
+For example, for the simplest condition, there're 3 missionaries and 3 cannibles, and the boat's volume is 2, in classic problem, there're at most 3 possible combinations: mc->, cc->, or c->(this one can be ommited because the cannible need to row the boat back, so returning to the original state), mm-> or m-> are not possible because the missionaries left behind will get eaten. 
+
+But, in this extended problem, suppose each missionary's rowing time is 1, 2, 3, and each cannible's time is 4, 5, 6, we have at most 15 possible combinations,
+m1c4, m1c5, m1c6, m2c4, m2c5, m2c6, m3c4, m3c5, m3c6, c4c5, c4c6, c5c6, c4, c5, c6(these 3 can be discarded, same reason as above).
+
+As we have learned from the classic problem, it will need 11 runs to cross the river safely, so, the total combination space is huge! So, BFS algorithm is almost impossible, and, although DFS algorithm is much more efficient than BFS, yet, because of the scale of the complexity, althouth this algorithm can deal with any number of missionaries and cannibles, in practical, the number of missionary and cannibles are limited, especially when boat's volume is increased. My experience is not to set missionaries and cannibles's number bigger than 7 with a boat volume is 4.
+
+One thing to note is that, I made thia program to return ALL possible solutions, trying to find how big the solution space could be. Actually I could setup a global minimal time, and eliminate lots of unnecessary solutions, this can largely increase the speed. 
+
+Another thing to note is that, because of the scale of the complexity, when it's not practical to find the optimal solution, I could use some other algorithms to find some nearly-optimal solution, which I am going to cover in following projects.
+
+
+
