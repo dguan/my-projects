23175c7191062bc4cc0cbbd1d51af47624ee4782
dguan <gdyxgzy@hotmail.com>
2015-01-02 14:12:16 +1100

    implemented a Singleton Design Pattern wrapper, which can turn any defaultly constructible class into a singleton.

---
+++ OOAD/DesignPatterns/Singleton/singleton.h
@@ -0,0 +1,58 @@
+#ifndef _SINGLETON_H_
+#define _SINGLETON_H_
+
+#include <mutex>
+
+//
+// If you would like to make a class to be a singleton,
+// you should always the Singleton<> wrapper to get the
+// ONE AND ONLY ONE instance of that class. To do this,
+// this wrapper requires that the object is defaultly 
+// constructible.
+//
+// This Singleton<MyClass> warpper will guarentee that:
+// If you use its GetInstance() method, you will always
+// get the same ONE AND ONLY ONE instance of MyClass.
+//
+// Please note that: this Singleton<> wrapper works
+// perfectly in Multi-threaded environments.
+//
+
+
+template<class ObjType>
+class SingletonWrapper
+{
+public:
+	virtual ~SingletonWrapper() {}
+	static ObjType *GetInstance();
+private:
+	static ObjType *m_instance;
+	// Mutex to protect multi threaded construction of ObjType
+	static std::mutex m_mutex;
+};
+
+template<class ObjType>
+ObjType *SingletonWrapper<ObjType>::GetInstance()
+{
+	// this lock only locks once when building the only ObjType instance.
+	// When running in multi-threaded environments, multiple threads may
+	// call GetInstance simultaneously, causing multiple ObjType instance
+	// to be created. It will not lock anymore after the initial instantiation.
+	if (m_instance == 0)
+	{
+		std::lock_guard<std::mutex> lk(m_mutex);
+
+		if (m_instance == 0)
+		{
+			static ObjType obj;	// Create ObjType on stack, ObjType must be default constructible
+			m_instance = &obj;
+		}
+	}
+	return m_instance;
+}
+
+// instantiation of internal static members
+template< class ObjType > ObjType *SingletonWrapper<ObjType>::m_instance = 0;
+template< class ObjType > std::mutex SingletonWrapper<ObjType>::m_mutex;
+
+#endif
---
+++ OOAD/DesignPatterns/Singleton/test_singleton.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+#include "singleton.h"
+
+class MyClass
+{
+public:
+	MyClass() {};
+
+	void Initialize(const std::string& name) { m_name = name; };
+	std::string GetName(void) const { return m_name; };
+	void SetName(const std::string& nm) { m_name = nm; };
+private:
+	std::string m_name;
+	
+	MyClass(const MyClass&) = delete;
+	MyClass& operator = (MyClass &) = delete;
+};
+
+int main()
+{
+	// The following 2 lines still work, but this is not
+	// the way MyClass is supposed to work.
+	MyClass mc0;
+	MyClass *pmc0 = new MyClass();
+	
+	//
+	// If you would like to make a class to be a singleton,
+	// you should always the Singleton<> wrapper to get the
+	// ONE AND ONLY ONE instance of that class. To do this,
+	// this wrapper requires that the object is defaultly 
+	// constructible.
+	//
+	// This Singleton<MyClass> warpper will guarentee that:
+	// If you use its GetInstance() method, you will always
+	// get the same ONE AND ONLY ONE instance of MyClass.
+	//
+	// Please note that: this Singleton<> wrapper works
+	// perfectly in Multi-threaded environments.
+	//
+	SingletonWrapper<MyClass> singleton_MyClass;
+	MyClass *pmc1 = singleton_MyClass.GetInstance();
+	pmc1->Initialize("12345");
+	
+	MyClass *pmc2 = SingletonWrapper<MyClass>::GetInstance();
+	pmc1->Initialize("abcde");
+	
+	MyClass *pmc3 = SingletonWrapper<MyClass>::GetInstance();
+	pmc2->SetName("UVWXYZ");
+	
+	std::cout << pmc1 << " --- " << pmc2 << " --- " << pmc3 << std::endl;
+	std::cout << pmc1->GetName() << " --- " << pmc2->GetName() << " --- " << pmc3->GetName() << std::endl;
+	
+	
+	delete pmc0;
+	return 0;
+}
